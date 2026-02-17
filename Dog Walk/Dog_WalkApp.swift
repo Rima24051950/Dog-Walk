50ea195f787aabfcbc84090b7e9abd8d53f45401
Rima <rimarz@yandex.ru>
2026-02-17 10:20:22 +0300

    Initial Commit

---
+++ Dog Walk/Dog_WalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_WalkApp.swift
+//  Dog Walk
+//
+//  Created by MacBook on 17.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Dog_WalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
